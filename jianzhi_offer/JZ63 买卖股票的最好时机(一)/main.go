8f7e2612bed6d211e0ac2f3b85b8bec61573f02a
howiechen <howiechen95@gmail.com>
2022-03-16 09:43:16 +0800

    update

---
+++ jianzhi_offer/JZ63 买卖股票的最好时机(一)/main.go
@@ -0,0 +1,24 @@
+package main
+
+func maxProfit(prices []int) int {
+	if len(prices) <= 1 {
+		return 0
+	}
+
+	n := len(prices)
+	dp := make([][2]int, n+1)
+	dp[0][1] = -prices[0]
+
+	for i := 1; i < n; i++ {
+		dp[i][0] = max(dp[i-1][0], dp[i-1][1]+prices[i])
+		dp[i][1] = max(dp[i-1][1], -prices[i])
+	}
+	return dp[n-1][0]
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
