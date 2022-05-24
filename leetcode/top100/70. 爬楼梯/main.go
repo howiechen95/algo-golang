bc7571abc5437df115116ecc97ece02acea84a08
howiechen <howiechen95@gmail.com>
2022-05-25 07:23:05 +0800

    update

---
+++ leetcode/top100/70. 爬楼梯/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"math"
+)
+
+func climbStairs(n int) int {
+	sqrt5 := math.Sqrt(5)
+	pow1 := math.Pow((1+sqrt5)/2, float64(n+1))
+	pow2 := math.Pow((1-sqrt5)/2, float64(n+1))
+	return int(math.Round((pow1 - pow2) / sqrt5))
+}
