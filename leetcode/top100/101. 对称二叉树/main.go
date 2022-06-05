cc00a077e2f17019ac70bd72ffe421951326dc80
howiechen <howiechen95@gmail.com>
2022-06-06 07:35:01 +0800

    update

---
+++ leetcode/top100/101. 对称二叉树/main.go
@@ -0,0 +1,33 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func isSymmetric(root *TreeNode) bool {
+	u, v := root, root
+	q := []*TreeNode{}
+	q = append(q, u)
+	q = append(q, v)
+	for len(q) > 0 {
+		u, v = q[0], q[1]
+		q = q[2:]
+		if u == nil && v == nil {
+			continue
+		}
+		if u == nil || v == nil {
+			return false
+		}
+		if u.Val != v.Val {
+			return false
+		}
+		q = append(q, u.Left)
+		q = append(q, v.Right)
+
+		q = append(q, u.Right)
+		q = append(q, v.Left)
+	}
+	return true
+}
