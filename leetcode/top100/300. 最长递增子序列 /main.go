cfb9f83219567f828bdd25cb01df3ac351a7f3cd
howiechen <howiechen95@gmail.com>
2022-07-18 08:09:16 +0800

    update

---
+++ leetcode/top100/300. 最长递增子序列 /main.go
@@ -0,0 +1,24 @@
+package main
+
+func lengthOfLIS(nums []int) int {
+	dp := []int{}
+	for _, num := range nums {
+		if len(dp) == 0 || dp[len(dp)-1] < num {
+			dp = append(dp, num)
+		} else {
+			l, r := 0, len(dp)-1
+			pos := r
+			for l <= r {
+				mid := (l + r) >> 1
+				if dp[mid] >= num {
+					pos = mid
+					r = mid - 1
+				} else {
+					l = mid + 1
+				}
+			}
+			dp[pos] = num
+		} //二分查找
+	}
+	return len(dp)
+}
