5828c473707b2b2ceb4e3eb94b99a83d46686286
howiechen <howiechen95@gmail.com>
2022-09-04 23:31:05 +0800

    update

---
+++ leetcode/200/27. 移除元素 /main.go
@@ -0,0 +1,12 @@
+package main
+
+func removeElement(nums []int, val int) int {
+	left := 0
+	for _, v := range nums { // v 即 nums[right]
+		if v != val {
+			nums[left] = v
+			left++
+		}
+	}
+	return left
+}
