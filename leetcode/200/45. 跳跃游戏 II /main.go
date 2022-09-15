263e1c8f6d367032405c4d23c0a9210d5098d2a1
howiechen <howiechen95@gmail.com>
2022-09-15 08:22:44 +0800

    update

---
+++ leetcode/200/45. 跳跃游戏 II /main.go
@@ -0,0 +1,23 @@
+package main
+
+func jump(nums []int) int {
+	length := len(nums)
+	end := 0
+	maxPosition := 0
+	steps := 0
+	for i := 0; i < length-1; i++ {
+		maxPosition = max(maxPosition, i+nums[i])
+		if i == end {
+			end = maxPosition
+			steps++
+		}
+	}
+	return steps
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
