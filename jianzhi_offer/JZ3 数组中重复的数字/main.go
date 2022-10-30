cb77dc722a74553b373a20b7f9b57dadc2a65407
howiechen <howiechen95@gmail.com>
2022-10-30 21:46:52 +0800

    update

--- jianzhi_offer/JZ3 数组中重复的数字/main.go
+++ jianzhi_offer/JZ3 数组中重复的数字/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 /**
  * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
  *
@@ -16,4 +18,35 @@ func duplicate(numbers []int) int {
 		count[n]++
 	}
 	return -1
+}
+
+func findRepeatNumber(nums []int) int {
+	var a = nums[0]
+	for i := 0; i < len(nums); i++ {
+		if i != nums[i] {
+			a = nums[i]
+			break
+		}
+	}
+
+	for {
+		fmt.Println(nums)
+		if nums[a] == a {
+			return a
+		}
+		a, nums[a] = nums[a], a
+	}
+	return a
+}
+
+func main() {
+	testCases := [][]int{
+		//{0, 1, 2, 3, 4, 5, 5},
+		//{5, 4, 3, 2, 1, 1, 0},
+		//{3, 0, 4, 4, 2},
+		{3, 4, 2, 0, 0, 1},
+	}
+	for _, v := range testCases {
+		fmt.Println(findRepeatNumber(v))
+	}
 }
