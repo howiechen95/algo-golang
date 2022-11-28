5f05c03880fd28602a93837038ee32da32f126c7
howiechen <howiechen95@gmail.com>
2022-11-28 08:14:13 +0800

    update

---
+++ leetcode/200/99. 恢复二叉搜索树 /main.go
@@ -0,0 +1,31 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func recoverTree(root *TreeNode) {
+	stack := []*TreeNode{}
+	var x, y, pred *TreeNode
+	for len(stack) > 0 || root != nil {
+		for root != nil {
+			stack = append(stack, root)
+			root = root.Left
+		}
+		root = stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		if pred != nil && root.Val < pred.Val {
+			y = root
+			if x == nil {
+				x = pred
+			} else {
+				break
+			}
+		}
+		pred = root
+		root = root.Right
+	}
+	x.Val, y.Val = y.Val, x.Val
+}
