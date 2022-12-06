b80a340708faf6e460ac0c9e04f930aacc012e1b
howiechen <howiechen95@gmail.com>
2022-12-07 05:54:46 +0800

    update

---
+++ leetcode/200/110. 平衡二叉树 /main.go
@@ -0,0 +1,37 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func isBalanced(root *TreeNode) bool {
+	return height(root) >= 0
+}
+
+func height(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	leftHeight := height(root.Left)
+	rightHeight := height(root.Right)
+	if leftHeight == -1 || rightHeight == -1 || abs(leftHeight-rightHeight) > 1 {
+		return -1
+	}
+	return max(leftHeight, rightHeight) + 1
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -1 * x
+	}
+	return x
+}
